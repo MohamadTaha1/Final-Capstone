c0573f4eaf7353f2a7bced7601e7a4a4c2b05089
babybleep <alkhatibhanine@gmail.com>
2023-12-20 17:18:08 +0200

    removed user subscription from the orders page

--- React/src/features/delivery/OrdersPage.jsx
+++ React/src/features/delivery/OrdersPage.jsx
@@ -1,30 +1,16 @@
-import  { useEffect, useState } from 'react';
 import Navbar from "../../components/reusables/Navbar";
 import OrderDetails from "../../components/delivery/OrderDetails";
 import OrderHeader from "../../components/welcomer/OrderHeader";
 import Footer from "../../components/reusables/Footer";
-import SubscriptionDetails from "../../components/plans/UserShowSubscription";
 
 
 const OrderPage = () => {
-  const [userSubscription] = useState(null);
-
-  useEffect(() => {
-    // Replace this with your actual fetch call
-    const fetchSubscription = async () => {
-      // Fetch the subscription data and set it to state
-      // Example: setUserSubscription(fetchedData);
-    };
-
-    fetchSubscription();
-  }, []);
 
   return (
     <>
       <Navbar />
       <OrderHeader />
       <OrderDetails />
-      <SubscriptionDetails subscription={userSubscription} />
       <Footer />
     </>
   );
--- React/src/features/restaurant/RestaurantDisplay.jsx
+++ React/src/features/restaurant/RestaurantDisplay.jsx
@@ -15,7 +15,6 @@ const RestaurantDisplay = () => {
   return (
     <>
       {isLoggedIn ? <Navbar /> : <NavbarLoggedOut />}
-
       <RestaurantDetails />
       <Footer />
     </>
