d4083b5e98fd85fcfcf21a48d52f82c94eb1c952
babybleep <alkhatibhanine@gmail.com>
2023-12-22 13:33:59 +0200

    fixed orders from chef's page

--- React/src/components/AdminComponents/Orders.jsx
+++ React/src/components/AdminComponents/Orders.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, useCallback } from "react";
-import { useNavigate } from "react-router-dom";
 
 const getStatusColor = (status) => {
   switch (status) {
@@ -25,10 +24,8 @@ const getStatusBackground = (status) => {
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
-  const [showSuccessModal, setShowSuccessModal] = useState(false);
 
+  // Define fetchOrders inside useEffect or use useCallback
   const fetchOrders = useCallback(async () => {
     const token = localStorage.getItem("token");
     if (!token) {
@@ -55,97 +52,55 @@ const Orders = () => {
       setOrders(data.reverse());
     } catch (error) {
       console.error(error.message);
+      setOrders([]); // In case of an error, ensure orders is an empty array
     }
-  }, []); // Removed fetchOrders from dependency array
+  }, []);
 
   useEffect(() => {
     fetchOrders();
-  }, [fetchOrders]);
+  }, [fetchOrders]); // Now fetchOrders is correctly included in the dependency array
 
-  const confirmOrder = useCallback(
-    async (orderId) => {
-      const token = localStorage.getItem("token");
-      if (!token) {
-        console.error("No authentication token found");
-        return;
-      }
-      setLoading(true); // Start loading
+  const confirmOrder = async (orderId) => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      console.error("No authentication token found");
+      return;
+    }
 
-      try {
-        const response = await fetch(
-          `http://localhost:8000/api/restaurant/orders/${orderId}/confirm`,
-          {
-            method: "POST",
-            headers: {
-              Authorization: `Bearer ${token}`,
-              "Content-Type": "application/json",
-            },
-          }
-        );
-
-        if (!response.ok) {
-          throw new Error("Failed to update order status");
+    try {
+      const response = await fetch(
+        `http://localhost:8000/api/restaurant/orders/${orderId}/confirm`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
         }
+      );
 
-        const updatedOrder = await response.json();
-        setOrders((currentOrders) =>
-          currentOrders.map((order) =>
-            order.id === updatedOrder.order.id ? updatedOrder.order : order
-          )
-        );
-        setShowSuccessModal(true);
-
-        // Set a timeout of 5 seconds before navigating to /owner
-        setTimeout(() => {
-          navigate("/owner");
-        }, 5000); // 5000 milliseconds = 5 seconds
-      } catch (error) {
-        console.error(error.message);
-      } finally {
-        setLoading(false);
+      if (!response.ok) {
+        throw new Error("Failed to update order status");
       }
-    },
-    [navigate]
-  );
 
-  const handleSuccess = () => {
-    setShowSuccessModal(false);
-    navigate("/owner");
+      const updatedOrder = await response.json();
+      setOrders((currentOrders) =>
+        currentOrders.map((order) =>
+          order.id === updatedOrder.order.id ? updatedOrder.order : order
+        )
+      );
+    } catch (error) {
+      console.error(error.message);
+    }
   };
 
   return (
     <div className="container min-h-screen mx-auto p-6">
-      {/* Loading Screen */}
-      {loading && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-10 text-center">
-          <div className="bg-white p-5 rounded-lg shadow-xl">
-            <div className="flex flex-col justify-center items-center">
-              <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-primary"></div>
-              <p className="text-lg font-semibold mt-4">Loading</p>
-            </div>
-          </div>
-        </div>
-      )}
-
-      {/* Success Modal */}
-      {showSuccessModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-10">
-          <div className="bg-white p-8 rounded-lg shadow-xl text-center">
-            <h3 className="text-2xl font-semibold text-green-600">Success!</h3>
-            <p className="text-lg mt-2">Your review has been submitted.</p>
-            <button
-              onClick={handleSuccess}
-              className="bg-green-500 text-white mt-4 px-6 py-2 rounded-lg shadow hover:bg-green-600 transition-colors"
-            >
-              Go to Home
-            </button>
-          </div>
-        </div>
-      )}
       <h1 className="text-3xl font-inter text-center mb-10 text-text2">
         My Orders
       </h1>
-      {orders.length === 0 ? (
+
+      {Array.isArray(orders) && orders.length === 0 ? (
         <p className="text-center text-xl text-gray-600">No orders found</p>
       ) : (
         orders.map((order) => (
@@ -171,12 +126,13 @@ const Orders = () => {
             <div className="px-6 py-4">
               <h3 className="text-lg font-inter mb-3">Order Details:</h3>
               <ul className="list-disc list-inside">
-                {order.order_details.map((detail) => (
-                  <li key={detail.id} className="text-gray-700">
-                    {detail.dish.name} - Quantity: {detail.quantity}, Price: $
-                    {parseFloat(detail.price).toFixed(2)}
-                  </li>
-                ))}
+                {order.order_details &&
+                  order.order_details.map((detail) => (
+                    <li key={detail.id} className="text-gray-700">
+                      {detail.dish.name} - Quantity: {detail.quantity}, Price: $
+                      {parseFloat(detail.price).toFixed(2)}
+                    </li>
+                  ))}
               </ul>
             </div>
             {order.status === "pending" && (
