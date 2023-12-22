4929098156913acbfe003ceba28b33e1a217b3fb
babybleep <alkhatibhanine@gmail.com>
2023-12-22 12:18:29 +0200

    chef navigates to dashboard when order is confirmed

--- React/src/components/AdminComponents/Orders.jsx
+++ React/src/components/AdminComponents/Orders.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 
 const getStatusColor = (status) => {
@@ -26,12 +26,10 @@ const getStatusBackground = (status) => {
 const Orders = () => {
   const [orders, setOrders] = useState([]);
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
 
-  useEffect(() => {
-    fetchOrders();
-  }, []); // fetchOrders is missing in dependency array
-
-  const fetchOrders = async () => {
+  const fetchOrders = useCallback(async () => {
     const token = localStorage.getItem("token");
     if (!token) {
       console.error("No authentication token found");
@@ -58,47 +56,92 @@ const Orders = () => {
     } catch (error) {
       console.error(error.message);
     }
-  };
+  }, []); // Removed fetchOrders from dependency array
 
-  const confirmOrder = async (orderId) => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      console.error("No authentication token found");
-      return;
-    }
+  useEffect(() => {
+    fetchOrders();
+  }, [fetchOrders]);
 
-    try {
-      const response = await fetch(
-        `http://localhost:8000/api/restaurant/orders/${orderId}/confirm`,
-        {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to update order status");
+  const confirmOrder = useCallback(
+    async (orderId) => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        console.error("No authentication token found");
+        return;
       }
+      setLoading(true); // Start loading
 
-      const updatedOrder = await response.json();
-      setOrders((currentOrders) =>
-        currentOrders.map((order) =>
-          order.id === updatedOrder.order.id ? updatedOrder.order : order
-        )
-      );
+      try {
+        const response = await fetch(
+          `http://localhost:8000/api/restaurant/orders/${orderId}/confirm`,
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
-      // Navigate to /owner route upon success
-      navigate("/owner");
-    } catch (error) {
-      console.error(error.message);
-    }
+        if (!response.ok) {
+          throw new Error("Failed to update order status");
+        }
+
+        const updatedOrder = await response.json();
+        setOrders((currentOrders) =>
+          currentOrders.map((order) =>
+            order.id === updatedOrder.order.id ? updatedOrder.order : order
+          )
+        );
+        setShowSuccessModal(true);
+
+        // Set a timeout of 5 seconds before navigating to /owner
+        setTimeout(() => {
+          navigate("/owner");
+        }, 5000); // 5000 milliseconds = 5 seconds
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [navigate]
+  );
+
+  const handleSuccess = () => {
+    setShowSuccessModal(false);
+    navigate("/owner");
   };
 
   return (
     <div className="container min-h-screen mx-auto p-6">
+      {/* Loading Screen */}
+      {loading && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-10 text-center">
+          <div className="bg-white p-5 rounded-lg shadow-xl">
+            <div className="flex flex-col justify-center items-center">
+              <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-primary"></div>
+              <p className="text-lg font-semibold mt-4">Loading</p>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* Success Modal */}
+      {showSuccessModal && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-10">
+          <div className="bg-white p-8 rounded-lg shadow-xl text-center">
+            <h3 className="text-2xl font-semibold text-green-600">Success!</h3>
+            <p className="text-lg mt-2">Your review has been submitted.</p>
+            <button
+              onClick={handleSuccess}
+              className="bg-green-500 text-white mt-4 px-6 py-2 rounded-lg shadow hover:bg-green-600 transition-colors"
+            >
+              Go to Home
+            </button>
+          </div>
+        </div>
+      )}
       <h1 className="text-3xl font-inter text-center mb-10 text-text2">
         My Orders
       </h1>
